905b89bbbfd527e73a85c8460c1e17b47335c7fe
Robinson Regalado <rregalado17@gmail.com>
2019-08-26 21:53:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,8 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT category SUM(amount)
   FROM projects
-  JOIN 
+  JOIN pledges 
+  ON projects.id = pledges.project_id 
   "
 end
 
