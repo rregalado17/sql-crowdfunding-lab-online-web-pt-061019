a295d28544bd8d5274f970c0ee55f2e9acb1ae24
Robinson Regalado <rregalado17@gmail.com>
2019-08-26 19:17:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title 
    FROM projects 
-   INNER JOIN projects_id
+   INNER JOIN projects
    ON projects.id = id.pledges"
 end
 
