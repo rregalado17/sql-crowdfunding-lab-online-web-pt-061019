d5f5ea00af4f98b7aaf5d9a79f69ec1045f8c1e3
Robinson Regalado <rregalado17@gmail.com>
2019-08-26 18:51:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,69 @@
+INSERT INTO users VALUES (1, "John", 25);
+INSERT INTO users VALUES (2, "Bob", 48);
+INSERT INTO users VALUES (3, "Mark", 19);
+INSERT INTO users VALUES (4, "Sam", 22);
+INSERT INTO users VALUES (5, "Maria", 42);
+INSERT INTO users VALUES (6, "Penny", 21);
+INSERT INTO users VALUES (7, "Aylissa", 35);
+INSERT INTO users VALUES (8, "Alice", 36);
+INSERT INTO users VALUES (9, "Emily", 49);
+INSERT INTO users VALUES (10, "Peter", 58);
+INSERT INTO users VALUES (11, "Brann", 25);
+INSERT INTO users VALUES (12, "Clark", 74);
+INSERT INTO users VALUES (13, "Steve", 56);
+INSERT INTO users VALUES (14, "Ronald", 63);
+INSERT INTO users VALUES (15, "Dave", 47);
+INSERT INTO users VALUES (16, "Margie", 35);
+INSERT INTO users VALUES (17, "Lisa", 44);
+INSERT INTO users VALUES (18, "Witney", 27);
+INSERT INTO users VALUES (19, "Chris", 28);
+INSERT INTO users VALUES (20, "Johnny", 28);
+
+INSERT INTO projects VALUES (1, "Sound equipment", "music", 5000, "1/1/2015", "1/1/2016");
+INSERT INTO projects VALUES (2, "Yoga Retreat", "health", 1200, "5/1/2015", "8/1/2016");
+INSERT INTO projects VALUES (3, "Gym Membership", "fitness", 500, "1/1/2017", "6/1/2017");
+INSERT INTO projects VALUES (4, "Vitamins", "health", 100, "1/1/2015", "3/1/2015");
+INSERT INTO projects VALUES (5, "Web Design Startup", "creative", 10000, "1/1/2017", "1/1/2020");
+INSERT INTO projects VALUES (6, "Gym Equipment", "health", 500, "1/1/2017", "1/1/2019");
+INSERT INTO projects VALUES (7, "Instruments", "music", 20000, "1/1/2016", "1/1/2018");
+INSERT INTO projects VALUES (8, "Van", "music", 10000, "1/1/2013", "1/1/2014");
+INSERT INTO projects VALUES (9, "Design Concept", "creative", 50000, "11/13/2018", "1/21/2020");
+INSERT INTO projects VALUES (10, "Logo Design", "creative", 1100, "3/12/2017", "2/15/2019");
+
+INSERT INTO pledges VALUES (1, 50, 20, 3);
+INSERT INTO pledges VALUES (2, 5, 5, 2);
+INSERT INTO pledges VALUES (3, 20, 14, 2);
+INSERT INTO pledges VALUES (4, 100, 20, 3);
+INSERT INTO pledges VALUES (5, 87, 20, 10);
+INSERT INTO pledges VALUES (6, 233, 20, 8);
+INSERT INTO pledges VALUES (7, 75, 20, 2);
+INSERT INTO pledges VALUES (8, 50, 20, 5);
+INSERT INTO pledges VALUES (9, 200, 20, 7);
+INSERT INTO pledges VALUES (10, 10000, 20, 7);
+INSERT INTO pledges VALUES (11, 110, 20, 5);
+INSERT INTO pledges VALUES (12, 5000, 20, 8);
+INSERT INTO pledges VALUES (13, 2000, 20, 9);
+INSERT INTO pledges VALUES (14, 35, 20, 3);
+INSERT INTO pledges VALUES (15, 750, 20, 1);
+INSERT INTO pledges VALUES (16, 200, 20, 9);
+INSERT INTO pledges VALUES (17, 5500, 20, 9);
+INSERT INTO pledges VALUES (18, 3000, 20, 10);
+INSERT INTO pledges VALUES (19, 4500, 20, 7);
+INSERT INTO pledges VALUES (20, 600, 20, 7);
+INSERT INTO pledges VALUES (21, 45, 20, 6);
+INSERT INTO pledges VALUES (22, 95, 20, 8);
+INSERT INTO pledges VALUES (23, 100, 20, 6);
+INSERT INTO pledges VALUES (24, 65, 20, 1);
+INSERT INTO pledges VALUES (25, 20, 20, 1);
+INSERT INTO pledges VALUES (26, 10, 20, 5);
+INSERT INTO pledges VALUES (27, 900, 20, 9);
+INSERT INTO pledges VALUES (28, 10000, 20, 7);
+INSERT INTO pledges VALUES (29, 5500, 20, 8);
+INSERT INTO pledges VALUES (30, 116, 20, 3);
+
+
+
+
+
+
+
