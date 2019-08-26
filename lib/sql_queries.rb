aa4089e19ccf97784ecf471ab7a036bdd8c355f1
Robinson Regalado <rregalado17@gmail.com>
2019-08-26 20:42:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,11 +25,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(funding_goal)
+  "SELECT projects.title, SUM(pledges.amount) - funding_goal
    FROM projects 
    INNER JOIN pledges 
    ON projects.id = pledges.project_id
-   GROUP BY projects.title HAVING SUM(amount) > funding_goal
+   GROUP BY projects.id 
+   HAVING SUM(amount) > funding_goal
   "
 end
 
