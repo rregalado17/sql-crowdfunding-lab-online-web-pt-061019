0dae3a819c6afb41e66b7a83bc812510be632732
Robinson Regalado <rregalado17@gmail.com>
2019-08-26 20:38:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
    FROM projects 
    INNER JOIN pledges 
    ON projects.id = pledges.project_id
-   GROUP BY projects.title HAVING SUM(amount) > pledges.funding_goal
+   GROUP BY projects.title HAVING SUM(amount) > funding_goal
   "
 end
 
